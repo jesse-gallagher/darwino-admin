f549e6fdda8472ac8e3cc88cf0e069bddbe8a304
Jesse Gallagher <jesse@secondfoundation.org>
2019-04-18 10:37:23 -0400

    Remove currently-unused projects

--- darwino-admin/darwino-admin-swt-hybrid/src/main/java/com/darwino/admin/app/AppPlugin.java
+++ darwino-admin/darwino-admin-swt-hybrid/src/main/java/com/darwino/admin/app/AppPlugin.java
@@ -8,8 +8,9 @@ import java.util.List;
 
 import com.darwino.admin.Messages;
 import com.darwino.commons.platform.beans.ManagedBeansExtension;
+import com.darwino.commons.platform.impl.PluginImpl;
 
-public class AppPlugin extends AppMobilePlugin {
+public class AppPlugin extends PluginImpl {
 	
 	public AppPlugin() {
 		super(Messages.getString("AppPlugin.appName")); //$NON-NLS-1$
@@ -18,6 +19,7 @@ public class AppPlugin extends AppMobilePlugin {
 	@Override
 	public void findExtensions(Class<?> serviceClass, List<Object> extensions) {
 		super.findExtensions(serviceClass, extensions);
+		AppBasePlugin.findExtensions(serviceClass, extensions);
 		
 		if(serviceClass==ManagedBeansExtension.class) {
 			extensions.add(new HomeDirBeanExtension());
